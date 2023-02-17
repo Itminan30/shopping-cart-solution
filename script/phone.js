72caf9cd82e1bda29cc6b14dcfc41ef7248119d1
Itminan30 <itminan30@gmail.com>
2023-02-17 12:20:48 +0600

    phone.js and utilities.js added and some code is written in phone.js

---
+++ script/phone.js
@@ -0,0 +1,33 @@
+// Price of one Phone
+const singlePhonePrice = 1219;
+// function to update the phone number 
+function updatePhoneNumber(isIncrease){
+    // get the number from the phone no. input field
+    const phoneNumberField = document.getElementById('phone-number-field');
+    const previousPhoneNumber = parseInt(phoneNumberField.value);
+    // increase or decrease the phone no. according to the button pressed
+    let newPhoneNumber;
+    if(isIncrease){
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else{
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+    phoneNumberField.value = newPhoneNumber;
+    // return the new phone no.
+    return newPhoneNumber;
+}
+// function to update the total price of phone
+function updatePhonePrice(phoneTotalPrice){
+    document.getElementById('phone-total').innerText = phoneTotalPrice;
+}
+// events occur when + button is clicked
+document.getElementById('btn-phone-plus').addEventListener('click',function(){
+    //const newPhoneNumber = updatePhoneNumber(true);
+    updatePhonePrice(updatePhoneNumber(true)*singlePhonePrice);
+})
+// events occur when - button is clicked
+document.getElementById('btn-phone-minus').addEventListener('click',function(){
+    //const newPhoneNumber = updatePhoneNumber(false);
+    updatePhonePrice(updatePhoneNumber(false)*singlePhonePrice);
+})
