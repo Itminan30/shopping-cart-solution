3a6e68c3b91e08dfc4d7613c57ffb531fbb07d45
Itminan30 <itminan30@gmail.com>
2023-02-17 11:02:38 +0600

    case adding with + button complete

--- script/case.js
+++ script/case.js
@@ -1,3 +1,9 @@
 document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const caseNumberField = document.getElementById('case-number-field');
+    const previousCaseNumber = parseInt(caseNumberField.value);
     
+    const newCaseNumber = previousCaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
+
+
 })
