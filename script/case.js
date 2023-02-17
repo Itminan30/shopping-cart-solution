4224cfd13f8bddc11393aae460571c2533dfd1c9
Itminan30 <itminan30@gmail.com>
2023-02-17 11:38:20 +0600

    updateCasePrice function added and case.js almost finished

--- script/case.js
+++ script/case.js
@@ -1,23 +1,33 @@
+// Price of one Case
+const singleCasePrice = 59;
+// function to update the case number 
 function updateCaseNumber(isIncrease){
+    // get the number from the case no. input field
     const caseNumberField = document.getElementById('case-number-field');
     const previousCaseNumber = parseInt(caseNumberField.value);
-    
+    // increase or decrease the case no. acording to the button pressed
     let newCaseNumber;
-
     if(isIncrease){
         newCaseNumber = previousCaseNumber + 1;
     }
     else{
         newCaseNumber = previousCaseNumber - 1;
     }
-    
     caseNumberField.value = newCaseNumber;
+    // return the new case no.
+    return newCaseNumber;
 }
-
+// function to update the total price of case
+function updateCasePrice(caseTotalPrice){
+    document.getElementById('case-total').innerText = caseTotalPrice;
+}
+// events occur when + button is clicked
 document.getElementById('btn-case-plus').addEventListener('click', function(){
-    updateCaseNumber(true);
+    //const newCaseNumber = updateCaseNumber(true);
+    updateCasePrice(updateCaseNumber(true)*singleCasePrice);
 })
-
+// events occur when - button is clicked
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-    updateCaseNumber(false);
+    //const newCaseNumber = updateCaseNumber(false);
+    updateCasePrice(updateCaseNumber(false)*singleCasePrice);
 })
