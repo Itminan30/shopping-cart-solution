1d6dae4c05fb20bd6f2a2804b0a9795a796d232f
Itminan30 <itminan30@gmail.com>
2023-02-17 11:15:21 +0600

    used updateCaseNumber function to increase and decrease the case no value

--- script/case.js
+++ script/case.js
@@ -1,9 +1,23 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCaseNumber(isIncrease){
     const caseNumberField = document.getElementById('case-number-field');
     const previousCaseNumber = parseInt(caseNumberField.value);
     
-    const newCaseNumber = previousCaseNumber + 1;
+    let newCaseNumber;
+
+    if(isIncrease){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    
     caseNumberField.value = newCaseNumber;
+}
 
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    updateCaseNumber(true);
+})
 
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
 })
